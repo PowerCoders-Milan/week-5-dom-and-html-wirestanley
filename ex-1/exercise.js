265d87480348bb70db0fb21390a2eebb39b3968c
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-03-02 12:00:00 +0100

    exercise1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,12 @@
 // Use this function that is linked to the button to change the background
 
 function set_background() {
+    document.getElementsByTagName("p")[0]
+    .style.backgroundColor = "purple";
+    document.getElementsByTagName("p")[1]
+    .style.backgroundColor = "blue";
+    document.getElementsByTagName("p")[2]
+    .style.backgroundColor = "brown"; 
+    }
 
-}
+
